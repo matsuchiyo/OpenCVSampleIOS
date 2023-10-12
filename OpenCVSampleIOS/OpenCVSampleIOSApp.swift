dd7c648720ec4233af52e93f0928ec0295c48246
matsuchiyo <macho.macho.dev@gmail.com>
2023-10-12 12:20:33 +0900

    First commit

---
+++ OpenCVSampleIOS/OpenCVSampleIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCVSampleIOSApp.swift
+//  OpenCVSampleIOS
+//
+//  Created by 松島勇貴 on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct OpenCVSampleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# OpenCVSampleIOS
